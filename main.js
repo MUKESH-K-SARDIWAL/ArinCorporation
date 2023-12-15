4a6c658468f050d6f3a346e7cc3cae47a6c3b9e9
MUKESH-K-SARDIWAL <webarion07@gmail.com>
2023-12-15 14:56:39 +0530

    design_patran

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ $('.owl-carousel').owlCarousel({
     margin: 10,
     responsiveClass: true,
     nav:true,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
     responsive: {
         0: {
-            items: 1,
-            nav: true
+            items: 2,
+            nav: false,
+
         },
         600: {
-            items: 3,
+            items:3,
             nav: false
         },
         1000: {
